cfac0c8ab70a1a4e013929ee3584e752df456308
ayushshekhar21 <shekharayush121@gmail.com>
2026-07-07 12:28:24 +0530

    Add files via upload

---
+++ second_day.py
@@ -0,0 +1,32 @@
+# STRINGS
+# str1 = "Ayush"
+# str2 = "shekhar"
+# print (str1 + str2)
+
+# str1 = "Ayush"
+# len1 = len(str1)
+# str2 = "shekhar"
+# len2 = len(str2)
+# print(len1)
+# print(len2)
+# final_len = len1 + len2
+# print(final_len)
+# final_str = str1 + ""  + str2
+# print(final_str)
+
+# # INDEXING
+# str = "Ayush shekhar"
+# # chr = str[2]
+# print(chr)
+
+# # SLICING
+# str = "Ayush shekhar"
+# print(str[0:2])
+# str = "Ayush shekhar"
+# print(str[-5:])
+
+
+# name = input("enter your name:")
+# print("length of your name is",len(name))
+
+ 
