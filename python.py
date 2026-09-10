ac00e476a57b700680eab98ea41804cea1a69434
Ayush code's <shekharayush121@gmail.com>
2026-09-10 20:31:54 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,731 @@
+# name = "ayush"
+# age = 19
+# city = "jamshedpur"
+# print("My name is :", name)
+# print("My age is :", age)
+# print("My city is:", city)
+
+# price = 99.9
+# print(price)
+
+# college = "Netaji subhas university"
+# print("My college name is:",college)
+
+# a = 10
+# b = 20
+# sum = (a+b)
+# print(sum)
+
+# mobile  = 8709637982
+# print(type(mobile))
+# print(mobile )
+
+# x = True
+# print(type(x))
+
+# name = "ayush"
+# age = 19
+# height = 170.12
+# is_student = True
+# print(type(name))
+# print(type(age))
+# print(type(height))
+# print(type(is_student))
+
+# a = 15
+# b = 4
+# sum = (a+b)
+# diff = (a-b)
+# multiply = (a*b)
+# divison = (a/b)
+# print(sum)
+# print(diff)
+# print(multiply)
+# print(divison)
+
+# x = 10
+# y = 5
+# x = y
+# print(x)
+
+# a = 10 
+# b = 20
+# a = b
+# b = 15
+# print(a)
+# print(b)
+
+# first = int(input("enter first:"))
+# second = int(input("enter second:"))
+# sum = (first + second)
+# print(sum)
+
+
+
+# name = ("ayush")
+# print(name)
+
+# message = ("Hello pyhton")
+# print(message)
+
+# name = ("ayush")
+# print(len(name))
+
+# text = ("python")
+# ch = 'python'[5]
+# print(ch)
+
+# text = "python programming"
+# uppercase_text = text.upper()
+# print(uppercase_text)
+
+# text = "AYUSH SHEKHAR"
+# lowercase_text = text.lower()
+# print(lowercase_text)
+
+
+# text = "   Python Programming   "
+
+# clean_text = text.strip()
+
+# print(clean_text)
+
+# text = "ayushloveanisha"
+# print(text.count("a"))
+
+# text = "I Love Python"
+
+# new_text = text.replace("Pyhton", "Programming")
+
+# print(new_text)
+
+# text = "I love Python"
+
+# new_text = text.replace("Python", "Programming")
+
+# print(new_text)
+
+# num = int(input("enter a number:"))
+
+# if num>0:
+#     print("number is positive")
+# elif num<0:
+#     print("number is negative")
+# else:
+#     print("none")
+
+# num = int(input("enter a number:"))
+
+# if num/2 == 0:
+#     print("number is even")
+# elif num/2 != 0:
+#       print("number is odd")
+# else:
+#      ("number is 0")
+
+# age = int(input("enter your age:"))
+
+# if age>18:
+#     print("eligible for vote")
+# elif age<18:
+#     print("not eligible for vote")
+# else:
+#     print("none")
+
+# num = int(input("enter a number:"))
+
+# if num>10:
+#     print("number is greater than 10")
+# elif num<10:
+#     print("number is less that 10")
+# else:
+#     print("none")
+
+# marks = int(input("enter your marks:"))
+
+# if marks>=40:
+#     print("passed")
+# else:
+#     print("failed")
+
+# num = int(input("enter a number:"))
+
+# if num % 5 == 0:
+#     print("number is divisible by 5")
+# else:
+#     print("number is not divisible by 5")
+
+# num1 = int(input("enter number1:"))
+# num2 = int(input("enter number2:"))
+
+# if num1>num2:
+#     print("num1 is greater than num2")
+# elif num1<num2:
+#     print("num2 is greater than num1")
+# else:
+#     print("both are equal")
+
+# age = int(input("enter age1:"))
+
+# if age<13:
+#     print("person is child")
+# elif age<18:
+#     print("person is teenager")
+# else:
+#     print("person is adult")
+
+# marks = int(input("enter marks:"))
+
+# if marks>90:
+#     print("grade:A")
+# elif marks>75<90:
+#     print("grade:B")
+# elif marks>60<75:
+#     print("grade:C")
+# elif marks>50<60:
+#     print("grade:D")
+# elif marks>40<50:
+#     print("grade:E")
+# else:
+#     print("failed")
+
+# temp = int(input("enter temp:"))
+
+# if temp>40:
+#     print("hot")
+# elif temp>20<40:
+#     print("warm")
+# else:
+#     print("cold")
+
+
+
+# list = ["apple","banana","orange"]
+# print(list)
+
+# list = [1,2,3,4,5]
+# print(list)
+
+# colors = ["red","purple","blue","green"]
+# print(colors[1:3])
+
+# list = [1,2,3,4,5]
+# print(len(list))
+
+# list = [10,20,30,40,50]
+# print(list[3])
+
+# list  = [100,200,300,400,500]
+# list.insert(0,1000)
+# print(list)
+# print(len(list))
+
+# name = ["ayush","arpit","sidhu"]
+# print(name[0])
+# print(name[1])
+# print(name[2])
+
+# list = []
+# list.append(1)
+# list.append(2)
+# list.append(3)
+# print(list)
+
+# num = [10,20,30,40]
+# print(num[1])
+
+# list = [1,2,3,4,5,6,7,8,9]
+# list.append(10)
+# print(list)
+
+# list = [11,12,13,14,16,17,18]
+# list.insert(4,15)
+# print(list)
+
+# list = [11,12,13,14,16,17,18]
+# list.remove(13)
+# print(list)
+
+# list = [11,12,13,14,16,17,18]
+# list.pop(5)
+# print(list)
+
+# list = [12,32,34,21,33,4,55,6,66]
+# list.sort()
+# print(list)
+
+# list = [12,32,34,21,33,4,55,6,66]
+# list.sort(reverse=True)
+# print(list)
+
+# list = [1,2,3,2,4,2,5,2,6]
+# list.count(2)
+# print(list)
+
+# tuple = (1,2,3,4,5)
+# print(tuple[0])
+
+# tuple = (1,2,3,4,5,6,7,8,9,10,11,12,13,)
+# print(len(tuple))
+
+# students = ["ayush","shekhar","kumar","rahul","amit"]
+# students.append("ritesh")
+# students.remove("kumar")
+# print(students)
+
+# numbers = [10,5,25,100,23]
+# largest = max(numbers)
+# smallest = min(numbers)
+# print("largest number:", largest)
+# print("smallest number:", smallest)
+
+# numbers = [1,2,3,4,5]
+# total = sum(numbers)
+# print("sum of numbers:",total)
+
+# info = {
+#     "name": "ayush",
+#     "age" : "19",
+#     "subject": "python"
+
+# }
+# print(info)
+
+# info = {
+#     "name" : "ayush",
+#     "age" : "19",
+#     "college" : "neta ji shubas university"
+
+
+# }
+# print(info)
+
+# student = {
+#     "name": "ayush shekhar",
+#     "age" : "19",
+#     "city" : "jamshedpur"
+# }
+# print(student["name"])
+
+# marks = {
+#     "physics": "85",
+#     "chemistry" : "90",
+#     "maths" : "80"
+
+# }
+# print(marks["physics"])
+
+# student = {
+#     "name" : "ayush",
+#     "age" : "19"
+# }
+# student["city"] = "jamshedpur"
+# student["name"] = "sidhu"
+# del student["age"]
+# print(student)
+
+# student = {
+#     "name" : "ayush",
+#     "age" : "19",
+#     "subject" : "physics", "chemistry"
+#     "city" : "jamshedpur"
+# }
+# print(len(student))
+
+# student = {
+#     "name" : "ayush",
+#     "age" : "19",
+# }
+# if "name" in student:
+#     print("name key exist")
+# else:
+#     print("name key doesn't exist")
+
+# student = {
+#     "name" : "ayush",
+#     "age": "19",
+#     "gender" : "male"
+# }
+# print(student.keys())
+
+# student = {
+#     "name" : "ayush",
+#     "age": "19",
+#     "gender" : "male"
+# }
+# print(student.values())
+
+# student = {
+#     "name" : "ayush",
+#     "age": "19",
+#     "gender" : "male"
+# }
+# print(student.items())
+
+# student = {
+#     "name" : "ayush",
+#     "age": "19",
+#     "gender" : "male"
+# }
+# student.update({
+#     "city" : "jamshedpur",
+#     "college" : "nsu"
+
+# })
+# print(student)
+
+# dictionary = {
+#     "rice" : "40",
+#     "wheat" : "50",
+#     "sugar" : "75"
+# }
+# print(dictionary["sugar"])
+
+# sets = {1,2,3,4,5}
+# print(sets)
+
+# sets = {"apple","banana","orange"}
+# print(sets)
+# print(type(sets))
+
+# numbers = {1,2,3,4,5}
+# numbers.add(6)
+# print(numbers)
+
+# numbers = {1,2,3,4,5}
+# numbers.remove(5)
+# print(numbers)
+
+# numbers = {1,2,3,4,5}
+# numbers.discard(5)
+# print(numbers)
+
+# numbers = {1,2,3,4,5}
+# print(len(numbers))
+
+# numbers = {1,2,2,3}
+# print(numbers) 
+
+# nums = (1,2,3,4,5,6,7,8,9,10)
+# for el in nums:
+#     print(el)
+
+# nums = (10,9,8,7,6,5,4,3,2,1)
+# for el in nums:
+#     print(el)
+
+# for i in range(5):
+#     print("python")
+
+# for i in range(10):
+#     print(i)    
+
+# for i in range(for21):
+#     if(i%2 == 0):
+#         print(i)
+
+# for i in range(20):
+#     if(i%2 != 0):
+#         print(i)
+
+# for i in range(1,11):
+#     print("5" "x",i, "=",5*i)
+
+# num = int(input("enter a number:"))
+
+# for i in range(1,11):
+#     print(num,"x",i,"=",num*i)
+
+# fruits = ["apple","banana","orange","grapes","mango"]
+# for fruit in fruits:
+#     print(fruit)
+
+# numbers = (1,2,3,4,5)
+
+# for num in numbers:
+#     print(num*2)
+
+# list = [10,20,30,40,50,60,70,80,90,100]
+# total = 0
+
+# for num in list:
+#     total+= num
+# print("sum =",total)
+
+# str = ("ayushshekhar")
+
+# for ch in str:
+#     print(ch)
+
+# nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+# count = 0 
+
+# for num in nums:
+#     if num%2 == 0:
+#         count+=1
+
+# print("numbers of even number:",count)
+
+# i = 10
+# while i>=1:
+#     print(i)
+#     i-=1
+
+# number = 1
+
+# while number <= 20:
+#     if number % 2 == 0:
+#         print(number)
+#     number += 1
+
+# number = int(input("enter a number:"))
+
+# while number != 0:
+#     print("you entered:",number)
+#     number = int(input("enter a number:"))
+
+# print("program ended.")
+
+# def greet():
+#     print("hello python my name is ayush")
+# greet()
+
+# def say_hello():
+#     print("ayush")
+# say_hello()
+
+# def add(a, b):
+#     print(a+b)
+
+# add(8,7)
+
+# def subtract(a, b):
+#     print(a-b)
+
+# subtract(1999,1876)
+
+# def square(number):
+#     print(number**2)
+
+# square(8)
+
+# def even_odd(number):
+#     if(number%2 ==0):
+#         print("even")
+#     else:
+#         print("odd")
+
+# even_odd(8)
+
+# def welcome(name):
+#     print("welcome",name)
+# welcome("ayush")
+
+# def multiply(a,b):
+#     print(a*b)
+# multiply(99,8)
+
+# def multiply(a,b):
+#      print(a*b)
+# multiply(88,8)
+
+# def multiply(a,b):
+#      print(a*b)
+# multiply(77,8)
+
+# def multiply(a,b):
+#      print(a*b)
+# multiply(66,8)
+
+# def add(a, b):
+#     return a+b
+
+# result = add(2220,1110)
+# print(result)
+
+# def cube(number):
+#     return number*3
+
+# result = cube(3)
+# print(result)
+
+# def eligible_to_vote(age):
+#     if age>=18:
+#         return "eligible to vote"
+#     else:
+#         return "not eligible to vote"
+# print(eligible_to_vote(17))
+
+# def string_len(text):
+#     return len(text)
+
+# print(string_len("Ayush shekhar"))
+
+# def uppercase(text):
+#     return text.upper()
+# print(uppercase("ayush"))
+
+# def largest(a,b,c):
+#     return max(a,b,c)
+
+# print(largest(18,83,29))
+
+# def list_sum(numbers):
+#     return sum(numbers)
+
+# print(list_sum([10,20,30,40]))
+
+# def largest_element(numbers):
+#     return max(numbers)
+
+# print(largest_element([10,20,30,40,50]))
+
+# def palindrome(word):
+#     if word == word[::-1]:
+#         return "palindrome"
+#     else:
+#         return "not a palindrome"
+
+# print(palindrome("eve"))
+
+# def factorial(n):
+#     result = 1
+
+#     for i in range(1, n + 1):
+#         result = result * i
+
+#         return result
+
+# num = int(input("enter a number:"))
+# print("factorial =", factorial (num))
+
+# def factorial(n):
+#     result = 1
+
+#     for i in range(1, n + 1):
+#         result = result * i
+
+#     return result
+
+# num = int(input("Enter a number: "))
+# print("Factorial =", factorial(num))
+
+# def is_prime(n):
+#     if n <= 1:
+#         return False
+
+#     for i in range(2, n):
+#         if n % i == 0:
+#             return False
+
+#     return True
+
+
+# num = int(input("Enter a number: "))
+
+# if is_prime(num):
+#     print("Prime number")
+# else:
+#     print("Not a prime number")
+
+
+# def show(n):
+#     if (n == 11):
+#         return
+#     print(n)
+#     show(n+1)
+
+# show(1)
+
+# def factorial(n):
+#     if n == 0 or n ==1:
+#         return 1
+#     else:
+#         return n* factorial(n-1)
+
+# num = int(input("enter a number:")) 
+# print("factorial:",factorial (num))
+
+
+# def sum_n(n):
+#     if n == 0:
+#         return 0
+#     else:
+#         return n + sum_n(n - 1)
+
+
+# num = int(input("Enter a number: "))
+# print("Sum:", sum_n(num))
+
+# class Student:
+#     name = "karan"
+# S1 = Student()
+# print(S1.name)
+
+# class Student:
+#     pass
+# student1 = Student
+# print(Student)
+
+# class Car:
+#     pass
+# Car1 = Car()
+# Car2 = Car()
+
+# print(Car1)
+# print(Car2)
+
+# class Student:
+#     name = ("ayush")
+#     age = (18)
+
+# S1 = Student()
+# print(S1.name)
+# print(S1.age)
+
+# class Car:
+#     name = "toyota"
+#     model = "fortuner"
+#     year = 2018
+
+# Car1 = Car()
+# print(Car.name)
+# print(Car.model)
+# print(Car.year)
+
+# class Rectangle:
+#     length = 10
+#     breadth = 10
+
+#     def area(self):
+#         return self.length * self.breadth
+
+# rectangle1 = Rectangle()
+# print("Area of rectangle :",rectangle1.area())
+
+# class Circle:
+#     radius = 5
+
+#     def area(self):
+#         return 3.14* self.radius * self.radius
+# area1 = Circle()
+# print("Area of cirlce:",area1.area())
+
+
+# class BankAccount:
+#     balance = 100000
+
+#     def display_balance(self):
+#         print("Balance:",self.balance)
+
+# account1 = BankAccount()
+# account1.display_balance()
+
+# class Number:
+
+#     def square(self, num):
+#         return num*num
+
+# square1 = Number()
+# print(square1.square(5))
